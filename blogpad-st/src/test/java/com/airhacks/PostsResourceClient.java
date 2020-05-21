66a7eaa3dcfdbd92485a8052b33620f2c6bab7a4
Adam Bien <abien@adam-bien.com>
2020-05-21 11:30:01 +0200

    creation and fetch posts is working

--- blogpad-st/src/test/java/com/airhacks/PostsResourceClient.java
+++ blogpad-st/src/test/java/com/airhacks/PostsResourceClient.java
@@ -1,18 +1,29 @@
 
 package com.airhacks;
 
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  *
  * @author airhacks.com
  */
 @Path("posts")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public interface PostsResourceClient {
 
     @GET
-    public String posts();
+    public JsonArray all();
 
+    @PUT
+    public Response save(JsonObject post);
 
 }
--- blogpad-st/src/test/java/com/airhacks/PostsResourceIT.java
+++ blogpad-st/src/test/java/com/airhacks/PostsResourceIT.java
@@ -3,6 +3,11 @@ package com.airhacks;
 
 import java.net.MalformedURLException;
 import java.net.URI;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.ws.rs.core.Response;
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
 import org.eclipse.microprofile.rest.client.RestClientBuilder;
 import org.junit.Before;
@@ -15,19 +20,31 @@ import org.junit.Test;
 public class PostsResourceIT {
 
     private RestClientBuilder builder;
+    private PostsResourceClient client;
 
     @Before
     public void init() {
         URI baseURI = URI.create("http://localhost:9080/blogpad/resources");
         this.builder = RestClientBuilder.newBuilder().baseUri(baseURI);
+        this.client = this.builder.build(PostsResourceClient.class);
     }
 
     @Test
-    public void posts() throws MalformedURLException {
-        PostsResourceClient client = this.builder.build(PostsResourceClient.class);
+    public void fetchAllPosts() throws MalformedURLException {
         assertNotNull(client);
-        String result = client.posts();
+        JsonArray result = client.all();
         assertNotNull(result);
         System.out.println("- " + result);
     }
+
+    @Test
+    public void createPost() {
+        JsonObject post = Json.createObjectBuilder().
+                add("title", "hello").
+                add("content", "world").
+                build();
+        Response response = this.client.save(post);
+        assertEquals(response.getStatus(), 201);
+    }
+
 }

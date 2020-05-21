57fb0d5941f78f2dcec4b70ba5a1c28fac2d7196
Adam Bien <abien@adam-bien.com>
2020-05-21 11:08:53 +0200

    post saving change to PUT

--- blogpad/src/main/java/blogpad/posts/boundary/PostsResource.java
+++ blogpad/src/main/java/blogpad/posts/boundary/PostsResource.java
@@ -7,7 +7,7 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -26,7 +26,7 @@ public class PostsResource {
         return this.store.allFiles();
     }
 
-    @POST
+    @PUT
     public Response save(Post post) {
         String fileName = this.store.save(post);
         URI uri = URI.create(fileName);

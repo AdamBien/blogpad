9f254fc69cb31935eabb84e9af1ba8cde3aef464
Adam Bien <abien@adam-bien.com>
2020-05-25 19:03:14 +0200

    initial creation

---
+++ reactor-st/src/test/java/airhacks/JavaTest.java
@@ -0,0 +1,19 @@
+/*
+ */
+package airhacks;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author airhacks.com
+ */
+public class JavaTest {
+
+    @Test
+    public void hello() {
+        System.out.println("...");
+    }
+
+
+}

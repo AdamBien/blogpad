251165b9e3f4b42b01fc431a921d55ac4909fdd4
Adam Bien <abien@adam-bien.com>
2020-05-26 09:51:04 +0200

    resource renamed to "posts"

--- reactor/src/main/java/blogpad/reactor/posts/boundary/PostsResource.java
+++ reactor/src/main/java/blogpad/reactor/posts/boundary/PostsResource.java
@@ -3,6 +3,7 @@ package blogpad.reactor.posts.boundary;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -19,7 +20,7 @@ public class PostsResource {
     @GET
     @Produces(MediaType.TEXT_HTML)
     @Path("{title}")
-    public String post(String title) {
+    public String post(@PathParam("title") String title) {
         return this.reactor.react(title);
     }
 

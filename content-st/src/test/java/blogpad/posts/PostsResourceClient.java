43249b6caaef443f556c3d006810f20657ba9960
Adam Bien <abien@adam-bien.com>
2020-06-05 20:13:48 +0200

    default method for rest client added

--- content-st/src/test/java/blogpad/posts/PostsResourceClient.java
+++ content-st/src/test/java/blogpad/posts/PostsResourceClient.java
@@ -4,11 +4,13 @@ package blogpad.posts;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -22,7 +24,11 @@ import javax.ws.rs.core.Response;
 public interface PostsResourceClient {
 
     @GET
-    public JsonArray all();
+    public JsonArray recent(@QueryParam("max") @DefaultValue("-1") int max);
+
+    default JsonArray all() {
+        return this.recent(-1);
+    }
 
     @PUT
     public Response save(JsonObject post);

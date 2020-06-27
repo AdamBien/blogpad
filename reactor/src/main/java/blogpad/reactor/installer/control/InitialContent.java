41de4e23603e6866c39eecdbbbd4c9d9039a1ba1
Adam Bien <abien@adam-bien.com>
2020-06-27 06:43:44 +0200

    default post title changed to initial

--- reactor/src/main/java/blogpad/reactor/installer/control/InitialContent.java
+++ reactor/src/main/java/blogpad/reactor/installer/control/InitialContent.java
@@ -29,7 +29,7 @@ public class InitialContent {
                    "content":
                    "hello, world",
                    "title":
-                   "welcome to blogpad"
+                   "initial"
                }
                """;
     }

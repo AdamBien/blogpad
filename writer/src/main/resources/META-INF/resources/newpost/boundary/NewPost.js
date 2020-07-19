1a407ee2d72dda7081a66d531170603ffa58c069
Adam Bien <abien@adam-bien.com>
2020-07-19 18:37:22 +0200

    form created

--- writer/src/main/resources/META-INF/resources/newpost/boundary/NewPost.js
+++ writer/src/main/resources/META-INF/resources/newpost/boundary/NewPost.js
@@ -6,9 +6,15 @@ class NewPost extends AirElement {
     view() { 
         return html`
             <section>
-                hello
+            <form>
+                <input requred placeholder="title" name="title">
+                <textarea placeholder="content" name="content"></textarea>
+                <button>create</button>
+            </form>
             </section>
         `;
     }
+
+
 }
 customElements.define('b-newpost',NewPost);

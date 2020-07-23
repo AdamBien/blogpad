3382708899659956055000bb7d94ea68338f2d27
Adam Bien <abien@adam-bien.com>
2020-07-23 09:06:11 +0200

    communication reducer registered

--- writer/src/main/resources/META-INF/resources/store.js
+++ writer/src/main/resources/META-INF/resources/store.js
@@ -1,9 +1,11 @@
 import { createStore,combineReducers } from "./lib/redux.js";
 import { newPostReducer } from "./newpost/entity/NewPostReducer.js";
 import { editTemplatesReducer } from "./templates/entity/EditTemplatesReducer.js";
+import { communicationReducer } from "./backend/entity/CommunicationReducer.js";
 
 const root = combineReducers({
     posts: newPostReducer,
-    templates: editTemplatesReducer
+    templates: editTemplatesReducer,
+    communication: communicationReducer
 });
 export const store = createStore(root, {}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());

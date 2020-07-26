625f0d2bacdcc763dee0d50965b2be6df8102642
Adam Bien <abien@adam-bien.com>
2020-07-26 07:36:08 +0200

    reducer refactored

--- writer/src/main/resources/META-INF/resources/posts/entity/PostReducer.js
+++ writer/src/main/resources/META-INF/resources/posts/entity/PostReducer.js
@@ -1,16 +1,20 @@
 export const NEW_POST_TEXT_CHANGED = "NEW_POST_TEXT_CHANGED";
 
 export const newPostReducer = (state = { editedPost: {}}, action) => { 
-    const { type,payload } = action;
+    const { type } = action;
     switch (type) { 
-        case NEW_POST_TEXT_CHANGED: { 
-            return {
-                ...state,
-                editedPost: updateEditedPost(state, payload)
-            }
-        }            
+        case NEW_POST_TEXT_CHANGED:
+            newPostTextChanged(state, action);
     }
     return state;
+}
+
+const newPostTextChanged = (state, { payload }) => { 
+    return {
+        ...state,
+        editedPost: updateEditedPost(state, payload)
+    }
+
 }
 
 const updateEditedPost = ({editedPost}, { name, value }) => { 

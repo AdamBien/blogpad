6df590328550faad71226814a5108c649c78d473
Adam Bien <abien@adam-bien.com>
2020-07-26 08:26:12 +0200

    post editor integrated

--- writer/src/main/resources/META-INF/resources/app.js
+++ writer/src/main/resources/META-INF/resources/app.js
@@ -1,11 +1,11 @@
 import { Router } from "./lib/@vaadin/router.js";
-import './posts/boundary/NewPost.js';
+import './posts/boundary/PostEditor.js';
 import './templates/boundary/EditTemplates.js';
 
 const outlet = document.querySelector('output');
 const router = new Router(outlet);
 router.setRoutes([
-  {path: '/',     component: 'b-newpost'},
+  {path: '/',     component: 'b-posteditor'},
   {path: '/templates',  component: 'b-edittemplates'},
   {path: '(.*)', component: 'b-navigationerror'},
 ]);

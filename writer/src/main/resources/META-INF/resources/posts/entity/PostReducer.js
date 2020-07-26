e35656bca9cc18a5b4d444b067e8bdd42d017eef
Adam Bien <abien@adam-bien.com>
2020-07-26 07:41:57 +0200

    allsPostsFetched implemented

--- writer/src/main/resources/META-INF/resources/posts/entity/PostReducer.js
+++ writer/src/main/resources/META-INF/resources/posts/entity/PostReducer.js
@@ -1,12 +1,22 @@
 export const NEW_POST_TEXT_CHANGED = "NEW_POST_TEXT_CHANGED";
+export const ALL_POSTS_FETCHED = "ALL_POSTS_FETCHED";
 
-export const newPostReducer = (state = { editedPost: {}}, action) => { 
+export const newPostReducer = (state = { editedPost: {},list:[]}, action) => { 
     const { type } = action;
     switch (type) { 
         case NEW_POST_TEXT_CHANGED:
             newPostTextChanged(state, action);
+        case ALL_POSTS_FETCHED:
+            allPostsFetched(state,action);
     }
     return state;
+}
+
+const allPostsFetched = (state, { payload }) => { 
+    return {
+        ...state,
+        list: payload
+    }
 }
 
 const newPostTextChanged = (state, { payload }) => { 
